46a06553b4eeca11836c133272f5c062342c2997
Ardito Hartoto <dhartoto@gmail.com>
2015-06-26 19:33:34 +1000

    add failure path for blank file and no file

--- lib/input_file.rb
+++ lib/input_file.rb
@@ -12,12 +12,35 @@ class InputFile
   end
 
   def self.import
-    resp = File.read(FULL_PATH)
-    resp = resp.split(/\n/)
-    new(content: resp)
+    if not file_present?
+      msg = "Input file has not been uploaded. Please save your file"\
+        " in the 'input' folder, type 'cont' then click enter."
+      new(error_message: msg)
+    elsif import_file.empty?
+      msg = "Input file is blank. Please add employee details to file, type"\
+       " 'cont' then click enter."
+      new(error_message: msg)
+    else
+      new(content: build_content)
+    end
   end
 
   def successful?
     !!content
+  end
+
+  private
+
+  def self.file_present?
+    File.exists?(FULL_PATH)
+  end
+
+  def self.import_file
+    File.read(FULL_PATH)
+  end
+
+  def self.build_content
+    file = import_file
+    file.split(/\n/)
   end
 end
--- spec/lib/input_file_spec.rb
+++ spec/lib/input_file_spec.rb
@@ -7,9 +7,11 @@ describe InputFile do
   describe '.import' do
 
     context 'csv file and content present' do
+
       let(:input_data) { "David,Rudd,60050,9%,01 March – 31 March\nRyan,Chen,120000,10%,01 March – 31 March\n" }
+
       before do
-        # allow(File).to receive_messages(input_data: input_data)
+        allow(File).to receive_messages(exists?: true, read: input_data)
       end
 
       it 'should returns nil for error_message' do
@@ -38,11 +40,41 @@ describe InputFile do
     context 'file not present' do
 
       before do
-        allow(File).to receive(:present?) { false }
+        allow(File).to receive(:exists?) { false }
       end
       it 'responds false to successful?' do
         resp = InputFile.import
-        expect(resp).to be_successful
+        expect(resp).not_to be_successful
+      end
+      it 'should have return nil for content' do
+        resp = InputFile.import
+        expect(resp.content).to be_nil
+      end
+      it 'should return error message' do
+        resp = InputFile.import
+        msg = "Input file has not been uploaded. Please save your file"\
+          " in the 'input' folder, type 'cont' then click enter."
+        expect(resp.error_message).to eq(msg)
+      end
+    end
+
+    context 'file is blank' do
+      before do
+        allow(File).to receive_messages(exists?: true, read: '')
+      end
+      it 'responds false to successful?' do
+        resp = InputFile.import
+        expect(resp).not_to be_successful
+      end
+      it 'should have return nil for content' do
+        resp = InputFile.import
+        expect(resp.content).to be_nil
+      end
+      it 'should return error message' do
+        resp = InputFile.import
+        msg = "Input file is blank. Please add employee details to file, type"\
+         " 'cont' then click enter."
+        expect(resp.error_message).to eq(msg)
       end
 
     end
