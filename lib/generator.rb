b7f521c384e97a8c859b2faafc3d2ccc22264e14
Ardito Hartoto <dhartoto@gmail.com>
2015-06-28 11:12:41 +1000

    Update Generator

--- lib/generator.rb
+++ lib/generator.rb
@@ -4,19 +4,20 @@ require 'payslip'
 
 class Generator
 
-  attr_accessor :staff, :payslips, :error_message, :success_message
+  attr_accessor :staff, :payslips, :error_message, :completion_message, :validation_errors
 
   def initialize
-    @staff           = Staff.create
-    @payslips        = Array.new
-    @success_message = "Payslips have been generated and may be retreived from the 'output' folder."
-    @error_message   = nil
+    @staff              = Staff.create
+    @payslips           = Array.new
+    @completion_message = ""
+    @validation_errors  = []
+    @error_message      = nil
   end
 
   # check error syntax import_error vs error_messages
   def run
     until staff.present?
-      self.error_message  = staff.error_message + "\n"
+      self.error_message  = staff.import_error + "\n"
       self.error_message += "Enter '1' to continue or '2' to exit the program."
       puts error_message
       input = gets.chomp
@@ -25,18 +26,32 @@ class Generator
     end
     generate_payslips
     deliver_output
-    puts success_message
-    # puts number of payslips and number of errors
+    puts completion_message
+    display_all_validation_errors
   end
 
   def generate_payslips
     staff.list.each do |employee|
-      payslips << Payslip.generate(employee)
+      self.payslips << Payslip.generate(employee)
     end
+    self.validation_errors += staff.validation_errors
+    set_completion_message
   end
 
   def deliver_output
     OutputFile.deliver(self)
+  end
+
+  private
+
+  def set_completion_message
+    self.completion_message = "#{payslips.count} payslip information were"\
+    " generated and delivered to the output folder with #{validation_errors.count}"\
+    " errors."
+  end
+
+  def display_all_validation_errors
+    validation_errors.each {|error| puts error}
   end
 
 end
--- spec/lib/generator_spec.rb
+++ spec/lib/generator_spec.rb
@@ -19,27 +19,21 @@ describe Generator do
       resp = Generator.new
       expect(resp.error_message).to be_nil
     end
-    it 'assigns success_message' do
-      msg = "Payslips have been generated and may be retreived from the 'output' folder."
-      resp = Generator.new
-      expect(resp.success_message).to eq(msg)
-    end
   end
 
   describe '#run' do
-
     let(:staff) { double('staff') }
     before { allow(Staff).to receive(:create) { staff } }
 
     context 'when staff is present' do
-
       let(:generator) { Generator.new }
 
       before do
-        allow(staff).to receive_messages(present?: true, list:  ['staff_1', 'staff_2'])
+        allow(staff).to receive_messages(present?: true,
+          list:  ['staff_1', 'staff_2'],
+          validation_errors: [])
         allow(Payslip).to receive(:generate) { 'pay_slip' }
       end
-
       it 'generates payslips' do
         generator.run
         expect(generator.payslips.count).to eq(2)
@@ -48,10 +42,31 @@ describe Generator do
         expect(OutputFile).to receive(:deliver)
         generator.run
       end
+
+      context 'when no validation error in input file.' do
+
+        it 'assigns completion message with 0 errors' do
+          msg = "2 payslip information were generated and delivered to the"\
+            " output folder with 0 errors."
+          generator.run
+          expect(generator.completion_message).to eq(msg)
+        end
+
+      end
+
+      context 'when 1 validation error in input file.' do
+        it 'assigns completion message with 1 error' do
+          allow(staff).to receive(:validation_errors) { ["some error"] }
+          msg = "2 payslip information were generated and delivered to the"\
+            " output folder with 1 errors."
+          generator.run
+          expect(generator.completion_message).to eq(msg)
+        end
+      end
+
     end
 
     context 'staff is not present' do
-
       let(:error_message) { "Input file has not been uploaded. Please save input file"\
         " into the 'input' folder." }
       let(:generator) { Generator.new }
@@ -59,17 +74,16 @@ describe Generator do
         before do
           allow(staff).to receive(:present?).and_return(false, true)
           allow(staff).to receive_messages(
-            error_message: error_message,
-            list:  ['staff_1', 'staff_2']
+            import_error: error_message,
+            list:  ['staff_1', 'staff_2'],
+            validation_errors: []
           )
         end
 
       it 'assigns error message to display' do
         allow(generator).to receive(:gets) { '1' }
         msg = error_message + "\nEnter '1' to continue or '2' to exit the program."
-
         generator.run
-
         expect(generator.error_message).to eq(msg)
       end
       it 'creates staff after user enters cont' do
